982ae1a0447f3335e2c3ceb66511e843c4bc8d66
Christian E. Millán-Hernández <40301742+christiane-millan@users.noreply.github.com>
2020-07-23 14:16:47 -0500

    Create aproximacion.py

---
+++ Introduccion-computo/programas_numericos/aproximacion.py
@@ -0,0 +1,17 @@
+# Ejemplo de un problema resuelto por aproximación
+# No se puede ser preciso y rápido a la vez
+# Obtner la raíz cuadrada de un número
+
+objetivo = int(input('Escoge un numero entero:'))
+epsilon = 0.01 #0.0001
+paso = epsilon**2
+respuesta = 0.0
+
+while abs(respuesta**2 - objetivo) >= epsilon and respuesta <= objetivo:
+    print(abs(respuesta**2 - objetivo))
+    respuesta +=paso
+
+if abs(respuesta**2 - objetivo) >= epsilon:
+    print(f'No se encontro la raiz cuadrada de {objetivo}')
+else:
+    print(f'La raiz cuadrada de {objetivo} es {respuesta}')
